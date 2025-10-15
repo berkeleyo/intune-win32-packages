1fd43e110ab1e9dafd10d8747baacdaff7f0ad17
Berkeley <118970067+berkeleyo@users.noreply.github.com>
2025-10-15 16:03:22 +0100

    Update detect.ps1

--- Kyocera/detect.ps1
+++ Kyocera/detect.ps1
@@ -1,3 +1,3 @@
 
 $path = "C:\Program Files\Kyocera\Kyocera Cloud Print and Scan\JcsUiAgent\JcsUiAgent.exe"
-if ((Test-Path $path) -and ((Get-Item $path).VersionInfo.FileVersion -eq "1.14.28216.1")) { exit 0 } else { exit 1 }
+if ((Test-Path $path) -and ((Get-Item $path).VersionInfo.FileVersion -eq "*version*")) { exit 0 } else { exit 1 }
