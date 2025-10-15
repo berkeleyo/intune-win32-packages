c2b8ed91d268f7cc7628288c5b499bc737f53c77
Berkeley <118970067+berkeleyo@users.noreply.github.com>
2025-10-15 16:02:58 +0100

    Update detect.ps1

--- NewTeams/detect.ps1
+++ NewTeams/detect.ps1
@@ -1,1 +1,1 @@
-Test-Path 'C:\\ProgramData\\GLL\\NewTeams.installed' | Out-Null; if ($?) { exit 0 } else { exit 1 }
+Test-Path 'C:\\ProgramData\\*ORG*\\NewTeams.installed' | Out-Null; if ($?) { exit 0 } else { exit 1 }
